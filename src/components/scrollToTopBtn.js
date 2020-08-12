561dac96e596cf544d2ce348c700cbd06f2055de
Anatol Kerbs <anker2702@gmail.com>
2020-08-12 12:05:40 +0200

    120820_2

--- src/components/scrollToTopBtn.js
+++ src/components/scrollToTopBtn.js
@@ -1,31 +1,7 @@
 import React, { useState, useEffect } from "react"
-import { makeStyles } from "@material-ui/core/styles"
+import "./scrollToTopBtn.css"
 import ExpandLessIcon from "@material-ui/icons/ExpandLess"
 import IconButton from "@material-ui/core/IconButton"
-
-const useStyles = makeStyles(theme => ({
-  toTop: {
-    zIndex: 999,
-    position: "fixed",
-    bottom: "3.5vh",
-    backgroundColor: "black",
-    color: "rgba(249,234,207)",
-    margin: 0,
-
-    "&:hover, &.Mui-focusVisible": {
-      transition: "0.3s",
-      color: "rgba(133,26,29)",
-      backgroundColor: "rgba(249,234,207)",
-    },
-    [theme.breakpoints.up("xs")]: {
-      left: "3%",
-      // backgroundColor: "rgb(220,220,220,0.7)",
-    },
-    [theme.breakpoints.up("lg")]: {
-      left: "6.5%",
-    },
-  },
-}))
 
 // const handleClick = () => {
 //   window[`scrollTo`]({ top: document.body.scrollHeight, behavior: `smooth` })
@@ -34,8 +10,6 @@ const useStyles = makeStyles(theme => ({
 // <Scroll showBelow={250} />
 
 const ScrollToTopBtn = () => {
-  const classes = useStyles()
-
   const [show, setShow] = useState(250 ? false : true)
 
   const handleScroll = () => {
@@ -63,8 +37,7 @@ const ScrollToTopBtn = () => {
         <IconButton
           size="small"
           onClick={handleClick}
-          className={classes.toTop}
-          aria-label="to top"
+          className="toTop"
           component="span"
         >
           <ExpandLessIcon />
