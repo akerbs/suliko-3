4c6e94232dec29a12dbee8c7d6d09c57888342ec
Anatol Kerbs <anker2702@gmail.com>
2020-09-02 16:46:20 +0200

    020920_10

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,22 +2,42 @@ import { useEffect } from "react"
 import { navigate } from "gatsby"
 const window = require("global/window")
 
-// console.log(window.navigator.language)
-
-const langUrl =
-  window.navigator.language.slice(0, 2) === "ru"
-    ? "/rus"
-    : window.navigator.language.slice(0, 2) === "ge"
-    ? "/geo"
-    : window.navigator.language.slice(0, 2) === "de"
-    ? "/deu"
-    : window.navigator.language.slice(0, 2) === "en"
-    ? "/eng"
-    : "/eng"
-
 export default () => {
   useEffect(() => {
-    navigate(langUrl)
+    if (window.navigator.language.slice(0, 2) === "ru") {
+      navigate("/rus")
+    } else if (window.navigator.language.slice(0, 2) === "de") {
+      navigate("/deu")
+    } else if (window.navigator.language.slice(0, 2) === "ge") {
+      navigate("/geo")
+    } else {
+      navigate("/eng")
+    }
   }, [])
   return null
 }
+
+// import { useEffect } from "react"
+// import { navigate } from "gatsby"
+
+// const window = require("global/window")
+
+// // console.log(window.navigator.language)
+
+// const langUrl =
+//   window.navigator.language.slice(0, 2) === "ru"
+//     ? "/rus"
+//     : window.navigator.language.slice(0, 2) === "ge"
+//     ? "/geo"
+//     : window.navigator.language.slice(0, 2) === "de"
+//     ? "/deu"
+//     : window.navigator.language.slice(0, 2) === "en"
+//     ? "/eng"
+//     : "/eng"
+
+// export default () => {
+//   useEffect(() => {
+//     navigate(langUrl)
+//   }, [])
+//   return null
+// }
