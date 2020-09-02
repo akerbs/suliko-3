3d9cc8602dd2a0cd1f02b2f4c79f3cf61aa6abbb
Anatol Kerbs <anker2702@gmail.com>
2020-09-02 15:35:01 +0200

    020920_2

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,22 @@ import { useEffect } from "react"
 import { navigate } from "gatsby"
 const window = require("global/window")
 
+// console.log(window.navigator.language)
+
+const langUrl =
+  window.navigator.language.slice(0, 2) === "ru"
+    ? "/rus"
+    : window.navigator.language.slice(0, 2) === "ge"
+    ? "/geo"
+    : window.navigator.language.slice(0, 2) === "de"
+    ? "/deu"
+    : window.navigator.language.slice(0, 2) === "en"
+    ? "/eng"
+    : "/eng"
+
 export default () => {
   useEffect(() => {
-    if (window.navigator.language.slice(0, 2) !== "ru") {
-      navigate("/rus")
-    } else if (window.navigator.language.slice(0, 2) !== "de") {
-      navigate("/deu")
-    } else if (window.navigator.language.slice(0, 2) !== "ge") {
-      navigate("/geo")
-    } else {
-      navigate("/eng")
-    }
+    navigate(langUrl)
   }, [])
   return null
 }
