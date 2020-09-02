050bd3bcb31494fecd36e22b0c84b02872f34351
Anatol Kerbs <anker2702@gmail.com>
2020-09-02 16:13:29 +0200

    020920_7

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,23 +2,7 @@ import React from "react"
 import "./layout.css"
 import { ThemeProvider } from "@material-ui/core/styles"
 import theme from "./theme"
-import CookiesBarDeu from "./cookiesBar-deu"
-import CookiesBarRus from "./cookiesBar-rus"
-import CookiesBarGeo from "./cookiesBar-geo"
-import CookiesBarEng from "./cookiesBar-eng"
-
-const window = require("global/window")
-
-const CookiesBar =
-  window.navigator.language.slice(0, 2) === "ru"
-    ? CookiesBarRus
-    : window.navigator.language.slice(0, 2) === "ge"
-    ? CookiesBarGeo
-    : window.navigator.language.slice(0, 2) === "de"
-    ? CookiesBarDeu
-    : window.navigator.language.slice(0, 2) === "en"
-    ? CookiesBarEng
-    : CookiesBarEng
+import CookiesBar from "./cookiesBar"
 
 const Layout = ({ children }) => {
   return (
