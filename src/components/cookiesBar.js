5cb936604337c74bd5a6b429bce7e3952841f63a
Anatol Kerbs <anker2702@gmail.com>
2020-09-02 16:32:55 +0200

    020920_8

--- src/components/cookiesBar.js
+++ src/components/cookiesBar.js
@@ -1,6 +1,18 @@
 import React from "react"
 import CookieConsent from "react-cookie-consent"
 import Typography from "@material-ui/core/Typography"
+const window = require("global/window")
+
+const lang =
+  window.navigator.language.slice(0, 2) === "ru"
+    ? "rus"
+    : window.navigator.language.slice(0, 2) === "ge"
+    ? "geo"
+    : window.navigator.language.slice(0, 2) === "de"
+    ? "deu"
+    : window.navigator.language.slice(0, 2) === "en"
+    ? "eng"
+    : "eng"
 
 export default function CookiesBar() {
   return (
@@ -8,7 +20,7 @@ export default function CookiesBar() {
       cookieName="gatsby-gdpr-google-tagmanager"
       location="bottom"
       style={{ background: "rgba(0,0,0, .8)" }}
-      buttonText="Alle akzeptieren"
+      buttonText={btnYes}
       buttonStyle={{
         backgroundColor: "white",
         color: "black",
@@ -16,7 +28,7 @@ export default function CookiesBar() {
       }}
       expires={150}
       enableDeclineButton
-      declineButtonText="Nur essenzielle"
+      declineButtonText={btnNo}
       declineButtonStyle={{
         backgroundColor: "black",
         color: "white",
@@ -43,12 +55,56 @@ export default function CookiesBar() {
       // expires={999}
       //  overlay
     >
-      <Typography variant="body1"> DATENSCHUTZEINSTELLUNGEN </Typography>
-      <Typography variant="caption">
-        Wir nutzen Cookies auf unserer Website. Einige von ihnen sind
-        essenziell, während andere uns helfen, diese Website und Ihre Erfahrung
-        zu verbessern.
-      </Typography>{" "}
+      <Typography variant="body1" id="cookiesTitle">
+        {cookiesTitle}
+      </Typography>
+      <Typography variant="caption">{cookiesText}</Typography>
     </CookieConsent>
   )
 }
+
+const btnYes =
+  lang === "rus"
+    ? "Принять все cookies"
+    : lang === "geo"
+    ? "მიიღეთ ყველა cookies"
+    : lang === "deu"
+    ? "Akzeptiere alle Cookies"
+    : lang === "eng"
+    ? "Accept all cookies"
+    : "Accept all cookies"
+
+const btnNo =
+  lang === "rus"
+    ? "Только необходимые"
+    : lang === "geo"
+    ? "მხოლოდ საჭიროა"
+    : lang === "deu"
+    ? "Nur essenzielle"
+    : lang === "eng"
+    ? "Only essentials"
+    : "Only essentials"
+
+const cookiesTitle =
+  lang === "rus"
+    ? "НАСТРОЙКИ КОНФИДЕНЦИАЛЬНОСТИ"
+    : lang === "geo"
+    ? "კონფიდენციალურობის პარამეტრები"
+    : lang === "deu"
+    ? "DATENSCHUTZEINSTELLUNGEN"
+    : lang === "eng"
+    ? "PRIVACY SETTINGS"
+    : "PRIVACY SETTINGS"
+
+const cookiesText =
+  lang === "rus"
+    ? `На нашем веб-сайте мы используем файлы cookie. Некоторые из них необходимы, а другие помогают нам улучшить этот веб-сайт и улучшить ваш опыт.`
+    : lang === "geo"
+    ? `ჩვენ ვიყენებთ ქუქი – ფაილებს ჩვენს ვებ – გვერდზე. ზოგიერთი მათგანი აუცილებელია, ზოგი კი დაგვეხმარება ამ ვებ – გვერდის გაუმჯობესებაში და თქვენი გამოცდილების გაუმჯობესებაში.`
+    : lang === "deu"
+    ? ` Wir nutzen Cookies auf unserer Website. Einige von ihnen sind
+    essenziell, während andere uns helfen, diese Website und Ihre Erfahrung
+    zu verbessern.`
+    : lang === "eng"
+    ? `We use cookies on our website. Some of them are essential, while others help us to improve this website and your experience.`
+    : `We use cookies on our website. Some of them are essential, while others help us to improve this website and your experience.`
