d55808af93ca6b05bc1c6ba19c3ebf403267f22a
Anatol Kerbs <anker2702@gmail.com>
2020-09-20 12:40:37 +0200

    200920_1

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,11 @@
 import React, { useState, createContext, useEffect, useMemo } from "react"
 import "./layout.css"
-
 import { ThemeProvider } from "@material-ui/core/styles"
 import theme from "./theme"
 // import CookiesBar from "./cookiesBar"
 import CssBaseline from "@material-ui/core/CssBaseline"
 // import { navigate } from "gatsby"
+
 const window = require("global/window")
 
 export const LanguageContext = createContext()
@@ -19,16 +19,16 @@ export const LanguageContext = createContext()
 // }
 // init()
 
-export const lang =
-  window.navigator.language.slice(0, 2) === "ru"
-    ? "RUS"
-    : window.navigator.language.slice(0, 2) === "de"
-    ? "DEU"
-    : window.navigator.language.slice(0, 2) === "en"
-    ? "ENG"
-    : window.navigator.language.slice(0, 2) === "ge"
-    ? "GEO"
-    : "ENG"
+// export const lang =
+//   window.navigator.language.slice(0, 2) === "ru"
+//     ? "RUS"
+//     : window.navigator.language.slice(0, 2) === "de"
+//     ? "DEU"
+//     : window.navigator.language.slice(0, 2) === "en"
+//     ? "ENG"
+//     : window.navigator.language.slice(0, 2) === "ge"
+//     ? "GEO"
+//     : "ENG"
 
 // function detectLanguage() {
 //   if (window.navigator.language.slice(0, 2) === "ru") {
@@ -45,32 +45,58 @@ export const lang =
 // }
 
 function Layout({ children }) {
-  const [actLanguage, setActLanguage] = useState(lang)
+  const [actLanguage, setActLanguage] = useState("")
 
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  useEffect(() => {
+    if (typeof window !== `undefined`) {
+      console.log("LANGUAGE: ", window.navigator.language.slice(0, 2))
 
-  // function init() {
-  //   window.onload = async function () {
-  //     console.log("TESTTT")
-  //     await alert("LOL")
-  //     // detectLanguage()
-  //     console.log("LANGUAGE: ", window.navigator.language.slice(0, 2))
-  //   }
-  // }
-  // init()
-  // useEffect(() => {
-  //   window.addEventListener("load", detectLanguage)
-
-  //   return () => {
-  //     window.removeEventListener("load", detectLanguage)
-  //   }
-  // }, [detectLanguage])
-
-  // window.onload = init
+      if (window.navigator.language.slice(0, 2) === "ru") {
+        setActLanguage("RUS")
+      } else if (window.navigator.language.slice(0, 2) === "de") {
+        setActLanguage("DEU")
+      } else if (window.navigator.language.slice(0, 2) === "en") {
+        setActLanguage("ENG")
+      } else if (window.navigator.language.slice(0, 2) === "ge") {
+        setActLanguage("GEO")
+      } else {
+        setActLanguage("ENG")
+      }
+    }
+  }, [])
 
   useEffect(() => {
     console.log("language changed", actLanguage)
   }, [actLanguage])
+
+  window.onload = function () {
+    console.log("TESTTT")
+
+    // detectLanguage()
+    // console.log("LANGUAGE: ", window.navigator.language.slice(0, 2))
+  }
+
+  // useEffect(() => {
+  //   window.onload = function () {
+  //     console.log("LANGUAGE: ", window.navigator.language.slice(0, 2))
+  //     // if (window.navigator.language.slice(0, 2) === "ru") {
+  //     //   setActLanguage("RUS")
+  //     // } else if (window.navigator.language.slice(0, 2) === "de") {
+  //     //   setActLanguage("DEU")
+  //     // } else if (window.navigator.language.slice(0, 2) === "en") {
+  //     //   setActLanguage("ENG")
+  //     // } else if (window.navigator.language.slice(0, 2) === "ge") {
+  //     //   setActLanguage("GEO")
+  //     // } else {
+  //     //   setActLanguage("ENG")
+  //     // }
+  //   }
+  // }, [window.onload])
+
+  // useEffect(() => {
+  //   console.log("language changed", actLanguage)
+  // }, [actLanguage])
 
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
